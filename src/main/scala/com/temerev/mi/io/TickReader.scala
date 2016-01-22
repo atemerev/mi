aa2349dc03e2a133d79df60491261f34620e81d1
Alexander Temerev <atemerev@algotrader.ch>
2016-01-23 00:59:42 +0100

    Lazy tick iterator reader

---
+++ src/main/scala/com/temerev/mi/io/TickReader.scala
@@ -0,0 +1,37 @@
+package com.temerev.mi.io
+
+import java.io.File
+import java.time._
+import java.time.temporal.ChronoUnit._
+
+import com.miriamlaurel.fxcore._
+import com.miriamlaurel.fxcore.instrument.Instrument
+import com.miriamlaurel.fxcore.market.Snapshot
+
+import scala.io.Source
+
+class TickReader(val dataDir: File) {
+
+  require(dataDir.isDirectory)
+
+  def getTicks(instrument: Instrument, startDate: LocalDate, endDate: LocalDate): Iterator[Snapshot] = {
+    val daysBetween = DAYS.between(startDate, endDate)
+    val weekdays = (0 to daysBetween.toInt)
+      .map(startDate.plusDays(_))
+      .filter(date => date.getDayOfWeek != DayOfWeek.SATURDAY
+        && date.getDayOfWeek != DayOfWeek.SUNDAY)
+    val files = weekdays.map(wd => new File(new File(dataDir, wd.toString),
+      instrument.toString.replaceFirst("/", "-") + ".txt"))
+    val streams = files.filter(_.exists()).map(Source.fromFile(_).getLines())
+    streams.foldLeft(Iterator[String]())(_ ++ _).map(Snapshot.fromCsv)
+  }
+}
+
+object TickReader extends App {
+  val reader = new TickReader(new File("/opt/data"))
+  val startDate = LocalDate.of(2015, Month.JULY, 15)
+  val endDate = LocalDate.of(2015, Month.OCTOBER, 23)
+  println("Loading ticks...")
+  val ticks = reader.getTicks(EURUSD, startDate, endDate)
+  println("Loaded: " + ticks.size)
+}
